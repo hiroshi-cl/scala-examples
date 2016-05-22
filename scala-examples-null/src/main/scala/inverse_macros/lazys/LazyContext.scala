df2439277e060d05bb8f8360610f3368089377aa
Hiroshi Yamaguchi <hiroshi_yamaguchi@csg.ci.i.u-tokyo.ac.jp>
2016-05-22 16:51:53 +0900

    remove unused codes

--- scala-examples-null/src/main/scala/inverse_macros/lazys/LazyContext.scala
+++
@@ -1,5 +0,0 @@
-package inverse_macros.lazys
-
-import scala.util.control.ControlThrowable
-
-final case class LazyContext[+A](body: () => A) extends ControlThrowable
--- scala-examples-null/src/main/scala/inverse_macros/lazys/package.scala
+++
@@ -1,5 +0,0 @@
-package inverse_macros
-
-package object lazys {
-  @inline final def defer[A](body: => A): A@lzy = throw LazyContext(() => body)
-}
