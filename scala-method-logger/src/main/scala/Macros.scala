0596cbe4e8cf02b0b3cfb1c0e153b9d6ebf43f3e
Hiroshi Yamaguchi <hiroshi_yamaguchi@csg.ci.i.u-tokyo.ac.jp>
2016-06-03 02:48:18 +0900

    copy & log + test

--- scala-method-logger/src/main/scala/Macros.scala
+++ scala-method-logger/src/main/scala/Macros.scala
@@ -9,8 +9,15 @@ class loggerBundle(val c: whitebox.Context) {
   def impl(annottees: Tree*): Tree = {
     val DefDef(mods, name, tparams, vparamss, tpt, rhs) :: Nil = annottees
     val newName = TermName(name + "$" + "log")
-    val created = DefDef(mods, name, tparams, vparamss, tpt,
-      q"$newName[..${tparams.map(_.name)}](...${vparamss.map(_.map(_.name))})")
+    val list = List(
+      q"val r = $newName[..${tparams.map(_.name)}](...${vparamss.map(_.map(_.name))})",
+      q"print(${name + " ("})",
+      q"print(..${vparamss.flatten.map(_.name)})",
+      q"print(${") = "})",
+      q"println(r)",
+      q"r"
+    )
+    val created = DefDef(mods, name, tparams, vparamss, tpt, q"..$list")
     val copied = DefDef(mods, newName, tparams, vparamss, tpt, rhs)
     q"$created; $copied; ()"
   }
--- scala-method-logger/src/test/scala/Test.scala
+++ scala-method-logger/src/test/scala/Test.scala
@@ -6,6 +6,20 @@ object Test extends App {
   @logger
   def bye(a: Int)(b: Int) = a
 
+  @logger
+  def ret(a: Int): Int = {
+    println("ret")
+    if(true)
+      return a
+
+    a
+  }
+
+  @logger
+  def poly[A](a: A) = a
+
   println(hello(10))
-  println(bye(10)(20))
+  println(bye(20)(30))
+  println(ret(30))
+  println(poly(40))
 }
