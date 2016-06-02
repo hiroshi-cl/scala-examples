a15965f3b3409bb4dfb02338527aef1f83d3ec69
Hiroshi Yamaguchi <hiroshi_yamaguchi@csg.ci.i.u-tokyo.ac.jp>
2016-06-03 02:34:33 +0900

    log

--- scala-method-logger/src/main/scala/Macros.scala
+++ scala-method-logger/src/main/scala/Macros.scala
@@ -3,12 +3,20 @@ import scala.language.experimental.macros
 import scala.annotation.StaticAnnotation
 
 class loggerBundle(val c: whitebox.Context) {
+
   import c.universe._
 
   def impl(annottees: Tree*): Tree = {
     val DefDef(mods, name, tparams, vparamss, tpt, rhs) :: Nil = annottees
-    println(name)
-    DefDef(mods, name, tparams, vparamss, tpt, rhs)
+    val list = List(
+      q"print(${name + " ("})",
+      q"print(..${vparamss.flatten.map(_.name)})",
+      q"val r = $rhs",
+      q"print(${") = "})",
+      q"println(r)",
+      q"r"
+    )
+    DefDef(mods, name, tparams, vparamss, tpt, q"..$list")
   }
 }
 
--- scala-method-logger/src/test/scala/Test.scala
+++ scala-method-logger/src/test/scala/Test.scala
@@ -3,5 +3,9 @@ object Test extends App {
   @logger
   def hello(a: Int) = a
 
+  @logger
+  def bye(a: Int)(b: Int) = a
+
   println(hello(10))
+  println(bye(10)(20))
 }
